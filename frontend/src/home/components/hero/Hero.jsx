ff882772d7116fe723ffe534e6a9f76e8e76250f
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-09-15 17:29:30 -0500

    Feat: agrega el componente de búsqueda

--- frontend/src/home/components/hero/Hero.jsx
+++ frontend/src/home/components/hero/Hero.jsx
@@ -1,4 +1,5 @@
 import "./hero.css";
+import Input from "../../../components/input/Input";
 
 const Hero = () => {
   return (
@@ -11,6 +12,16 @@ const Hero = () => {
           Explora becas de distintas universidades y encuentra oportunidades que
           se ajusten a tu carrera y necesidades.
         </p>
+        <div className="hero-buscador">
+          <Input
+            id="buscador"
+            type="text"
+            textoLabel="Buscar becas"
+            placeholder="Ingeniería, Medicina, Programación..."
+            className="buscador-input"
+          />
+          <button className="buscador-boton">Buscar</button>
+        </div>
       </div>
     </div>
   );
