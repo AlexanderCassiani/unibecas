54d4034c2196adb4295dfb194ec1256e7da2db4d
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-09-15 17:30:41 -0500

    Feat: agrega la seccion de informacion sobres las becas

--- frontend/src/home/layout/HomeLayout.jsx
+++ frontend/src/home/layout/HomeLayout.jsx
@@ -1,9 +1,43 @@
+import "./homeLayout.css";
 import Hero from "../components/hero/Hero";
+import infoBecasImg from "../../assets/icons/home/info-becas-icon.svg";
 
 const HomeLayout = () => {
+  const infoBecas = [
+    {
+      id: 1,
+      titulo: "200+",
+      description: "Becas para estudiar",
+    },
+    {
+      id: 2,
+      titulo: "20+",
+      description: "Universidades afiliadas",
+    },
+    {
+      id: 3,
+      titulo: "150+",
+      description: "Programas disponibles",
+    },
+  ];
+
   return (
-    <div>
+    <div className="home-layout">
       <Hero />
+
+      <section className="contenedor-info-becas">
+        {infoBecas.map((info) => (
+          <div key={info.id} className="info-becas">
+            <h2>
+              <span className="icon-info-becas">
+                <img src={infoBecasImg} alt="Icono de becas" />
+              </span>
+              <span>{info.titulo}</span>
+            </h2>
+            <p>{info.description}</p>
+          </div>
+        ))}
+      </section>
     </div>
   );
 };
