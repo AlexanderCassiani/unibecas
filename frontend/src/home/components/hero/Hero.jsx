2ba97a486b012ee818696bcfb748a7e0c26c80ae
AlexanderCassiani <alexandercassiani74@gmail.com>
2026-09-15 15:52:00 -0500

    Feat: agrega el componente Hero con estructura y estilos minimos

---
+++ frontend/src/home/components/hero/Hero.jsx
@@ -0,0 +1,19 @@
+import "./hero.css";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="hero-contenido">
+        <h1>
+          Encuentra tu <span>beca</span> ideal
+        </h1>
+        <p>
+          Explora becas de distintas universidades y encuentra oportunidades que
+          se ajusten a tu carrera y necesidades.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
